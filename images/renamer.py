ecab147d368ef096e3ddaf9ee91dead363ae445f
CharlesCbl <79850926+CharlesCbl@users.noreply.github.com>
2021-03-01 14:35:23 +0100

    Update renamer.py

--- images/renamer.py
+++ images/renamer.py
@@ -1,1 +1,9 @@
+import os
 
+currentPath = r'C:\Users\Charles\Documents\Cours\3A\DeepLearning\Projet2\images'
+dirList = os.scandir(os.path.join(currentPath,r'images/Albrecht_Durer'))
+
+for image in dirList:
+    print(image.name)
+    splits=str.split(image.name,'_Durer')
+    os.rename(os.path.join(currentPath,r'images/Albrecht_Durer',image.name), os.path.join(currentPath,r'images/Albrecht_Durer2','Albrecht_Durer_'+splits[1]))
